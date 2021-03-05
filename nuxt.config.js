72b6e4e997679028d9df034e5bfdaa1ea8f3c8ae
dustinle48 <ducthinh481994@gmail.com>
2021-03-04 19:18:30 -0500

    Add OG Image

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - dustin-portfolio',
-    title: 'dustin-portfolio',
+    titleTemplate: 'Dustin Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og.image', property: 'og:image', content: '/thinh.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
