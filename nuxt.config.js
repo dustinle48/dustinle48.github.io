0d9192fc51e7308c3ea4e1596d7f2d95a06174ad
dustinle48 <ducthinh481994@gmail.com>
2021-03-06 23:25:06 -0500

    OG image

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { hid: 'og.image', property: 'og:image', content: '/thinh.jpg' }
+      { property: 'og:image', content: '/thinh.jpg' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/Portrait.jpg' },
