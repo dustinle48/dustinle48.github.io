6cfd3a29ac92a5b7c42dc0ad0e581ddee64f1410
dustinle48 <ducthinh481994@gmail.com>
2021-03-06 23:35:16 -0500

    Add Description

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,14 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: 'Dustin Portfolio',
+    title: 'Dustin Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Dustin Online Portfolio' },
       { property: 'og:image', content: '/thinh.jpg' }
     ],
     link: [
